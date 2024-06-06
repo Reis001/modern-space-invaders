c8c4a8bd89d468c73160ac4d8fc7fde294be6780
Reis <samuelreisesilva06@gmail.com>
2024-06-06 14:32:37 -0400

    add init function in index.js

--- index.js
+++ index.js
@@ -41,4 +41,50 @@ let fpsInterval = 1000 / fps;
 
 let msPrev = window.performance.now();
 
+function init() {
+  player = new Player();
+  projectiles = [];
+  grids = [];
+  invaterProjectiles = [];
+  particles = [];
+  bombs = [];
+  powerUps = [];
+  frames = 0;
 
+  let keys = {
+    ArrowLeft: {
+      pressed: false
+    },
+    ArrowRight: {
+      pressed: false
+    },
+    Space: {
+      pressed: false
+    }
+  };
+
+  randomInterval = Math.floor(Math.random() * 500 + 500);
+
+  game = {
+    over: false,
+    active: true
+  };
+
+  score = 0;
+}
+for (let i = 0; i < 100; i++) {
+  particles.push(
+    new Particle({
+      position: {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height
+      },
+      velocity: {
+        x: 0,
+        y: 0.3
+      },
+      radius: Math.random() * 2,
+      color: "white"
+    })
+  );
+}
