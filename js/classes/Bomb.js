6d631726944ad3f862e521dbcfb956291cbdbaa7
Reis <samuelreisesilva06@gmail.com>
2024-06-13 16:31:55 -0400

    add formatting of project files

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -20,56 +20,57 @@ class Bomb {
     c.fillStyle = this.color;
     c.fill();
     c.restore();
-}
+  }
 
-update() {
+  update() {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
-    if(this.position.x < this.radius + this.velocity.x >= canvas. 
-        whidth || this.position.x - this.radius + this.velocity.x <=0
+    if (
+      this.position.x < this.radius + this.velocity.x >= canvas.whidth ||
+      this.position.x - this.radius + this.velocity.x <= 0
     ) {
-        this.velocity.x = -this.velocity.x;
-    } else if(
-        this.position.y + this.radius + this.velocity.y >= canvas.
-        height || this.position.y - this.radius + this.velocity.y <= 0
+      this.velocity.x = -this.velocity.x;
+    } else if (
+      this.position.y + this.radius + this.velocity.y >= canvas.height ||
+      this.position.y - this.radius + this.velocity.y <= 0
     )
-    this.velocity.y = -this.velocity.y;
-}
-explode() {
+      this.velocity.y = -this.velocity.y;
+  }
+  explode() {
     audio.bomb.play();
     this.active = true;
     this.velocity.x = 0;
     this.velocity.y = 0;
     gsap.to(this, {
-        radius: 200,
-        color: "red" 
+      radius: 200,
+      color: "red"
     });
     gsap.to(this, {
-        delay: 0.1,
-        opacity: 0,
-        duration: 0.15
+      delay: 0.1,
+      opacity: 0,
+      duration: 0.15
     });
-}
+  }
 }
 class PowerUp {
-    constructor({ position, velocity }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.radius = 15;
-    }
-    draw() {
-        c.beginPath();
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-        c.fillStyle = "yellow";
-        c.fill();
-        c.closePath();
-    }
-    
-    update() {
-        this.draw();
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-    }
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 15;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "yellow";
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
 }
